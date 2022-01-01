6946294ae347fdbfc151dbb2d5618b61c0d98ae4
尚杰 <sj@shangjiedeMacBook-Pro.local>
2022-01-01 17:34:16 +0800

    尝试 httphttp.ServeMux

--- main.go
+++ main.go
@@ -5,19 +5,25 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello,欢迎来到goblog222332</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+"<a href=\"http://baidu.com\">百度</a>")
+		fmt.Fprintf(w, "<h1>hello, 欢迎来到goblog!</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到:(</h1><p>如有疑惑，请联系我们1</p>")
+		fmt.Fprint(w, "<h1>请求页面未找到:(</h1>"+
+			"<p>如有疑惑，请联系我们</p>")
 	}
-
 }
+
+func aboutHandel(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用于记录编程笔记，如您有反馈或建议，请联系"+
+		"<a href=\"www.baidu.ciom\">sj</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandel)
 	http.ListenAndServe(":3000", nil)
 }
