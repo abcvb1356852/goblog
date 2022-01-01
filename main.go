b68c8856080bb205fd373323a487cd70b5584fc0
尚杰 <sj@shangjiedeMacBook-Pro.local>
2022-01-01 17:17:33 +0800

    404状态码

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+"<a href=\"http://baidu.com\">百度</a>")
 	} else {
-		fmt.Fprint(w, "<h1>请求页面未找到:(</h1><p>如有疑惑，请联系我们</p>")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>请求页面未找到:(</h1><p>如有疑惑，请联系我们1</p>")
 	}
 
 }
