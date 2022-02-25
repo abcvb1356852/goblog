1858e136d70cff46b3919a5802663e9933c660be
sj <362795776@qq.com>
2022-02-25 14:28:31 +0800

    创建用户

---
+++ app/models/user/user.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"goblog/app/models"
+	"goblog/pkg/logger"
+	"goblog/pkg/model"
+)
+
+// User 用户模型
+type User struct {
+	models.BaseModel
+
+	Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
+	Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
+	Password string `gorm:"column:password;type:varchar(255)"`
+}
+
+func (user *User) Create() (err error) {
+	if err = model.DB.Create(&user).Error; err != nil {
+		logger.LogError(err)
+		return err
+	}
+
+	return nil
+}
