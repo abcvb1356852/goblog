293353120077e6f099b22a3796e712364ccfcc2e
sj <362795776@qq.com>
2022-03-01 11:09:33 +0800

    配置和环境变量

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"goblog/bootstrap"
+	"goblog/config"
+
 	"goblog/middlewares"
+	c "goblog/pkg/config"
 	"goblog/pkg/logger"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func init() {
+	// 初始化配置信息
+	config.Initialize()
+}
 
+func main() {
+	// 初始化 SQL
 	bootstrap.SetupDB()
+
+	// 初始化路由绑定
 	router := bootstrap.SetupRoute()
 
-	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	err := http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 	logger.LogError(err)
 }
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+	"goblog/pkg/config"
 	"goblog/pkg/logger"
 
 	"gorm.io/gorm"
@@ -18,15 +20,29 @@ func ConnectDB() *gorm.DB {
 
 	var err error
 
-	config := mysql.New(mysql.Config{
-		DSN: "root:sj19920123@tcp(116.63.156.28:33061)/goblog?charset=utf8&parseTime=True&loc=Local",
+	// 初始化 MySQL 连接信息
+	gormConfig := mysql.New(mysql.Config{
+		DSN: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&loc=Local",
+			config.GetString("database.mysql.username"),
+			config.GetString("database.mysql.password"),
+			config.GetString("database.mysql.host"),
+			config.GetString("database.mysql.port"),
+			config.GetString("database.mysql.database"),
+			config.GetString("database.mysql.charset")),
 	})
 
-	/* 	// 准备数据库连接池
-	   	DB, err = gorm.Open(config, &gorm.Config{}) */
+	var level gormlogger.LogLevel
+	if config.GetBool("app.debug") {
+		// 读取不到数据也会显示
+		level = gormlogger.Warn
+	} else {
+		// 只有错误才会显示
+		level = gormlogger.Error
+	}
+
 	// 准备数据库连接池
-	DB, err = gorm.Open(config, &gorm.Config{
-		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	DB, err = gorm.Open(gormConfig, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(level),
 	})
 
 	logger.LogError(err)
