bffa94e52f779ce38367451c91a8f50f9991d074
sj <362795776@qq.com>
2022-03-01 16:21:56 +0800

    简单分页

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -5,8 +5,10 @@ import (
 	"goblog/app/models/user"
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
+	"goblog/pkg/pagination"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
+	"net/http"
 	"strconv"
 )
 
@@ -32,12 +34,20 @@ func Get(idstr string) (Article, error) {
 }
 
 // GetAll 获取全部文章
-func GetAll() ([]Article, error) {
+func GetAll(r *http.Request, perPage int) ([]Article, pagination.ViewData, error) {
+
+	// 1. 初始化分页实例
+	db := model.DB.Model(Article{}).Order("created_at desc")
+	_pager := pagination.New(r, db, route.Name2URL("home"), perPage)
+
+	// 2. 获取视图数据
+	viewData := _pager.Paging()
+
+	// 3. 获取数据
 	var articles []Article
-	if err := model.DB.Preload("User").Find(&articles).Error; err != nil {
-		return articles, err
-	}
-	return articles, nil
+	_pager.Results(&articles)
+
+	return articles, viewData, nil
 }
 
 // Link 方法用来生成文章链接
