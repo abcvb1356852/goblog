c6a830d85830aeff719ca4513eec9771419f01dd
sj <362795776@qq.com>
2022-03-02 13:40:42 +0800

    显示分类

--- app/models/category/category.go
+++ app/models/category/category.go
@@ -4,6 +4,7 @@ import (
 	"goblog/app/models"
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
+	"goblog/pkg/route"
 )
 
 // Category 文章分类
@@ -20,4 +21,18 @@ func (category *Category) Create() (err error) {
 		return err
 	}
 	return nil
+}
+
+// All 获取分类数据
+func All() ([]Category, error) {
+	var categories []Category
+	if err := model.DB.Find(&categories).Error; err != nil {
+		return categories, err
+	}
+	return categories, nil
+}
+
+// Link 方法用来生成文章链接
+func (c Category) Link() string {
+	return route.Name2URL("categories.show", "id", c.GetStringID())
 }
